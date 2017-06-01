4e39d70dafe1c738ee261e35fce30a78e9dac3d0
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2017-06-01 10:44:54 +0200

    add setup.py for distribution/installation via pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+        name='sample',
+        version='0.0.1',
+        description='Package to control the RoboSchool RaspiBot',
+
+        url='https://github.com/tuc-roboschool/raspibot',
+
+        author='The RoboSchool Team',
+        author_email='roboschool@hrz.tu-chemnitz.de',
+
+        license='MIT',
+
+        # TODO: add classifiers
+        # TODO: add keywords
+
+        packages=['raspibot'],
+
+        # there's no functionality yet, so no dependencies either
+        install_requires=[]
+)
