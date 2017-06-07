945fcb652f4222b8cf920e7cbac670535414c16f
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2017-06-07 12:56:19 +0200

    added docstring and nicer formatting to LCD demo

--- demos/lcd_demo.py
+++ demos/lcd_demo.py
@@ -1,10 +1,19 @@
-# coding: utf-8
+"""Demonstrates simple output to the LCD."""
+
+from time import sleep
 from LCD import Display
+
 d = Display()
 d.init()
+
 d.clear()
 d.print_string("Hallo Leander!")
 d.cursor_goto_line(1)
 d.print_string("Check' Flosse!")
+
+sleep(1.5)
+
+# there is no need for specialized output functions since Python's built-in
+# string formatting capabilities are more than sufficient
 d.clear()
 d.print_string("{0:d} {0:X} {0:b}".format(0xca))
