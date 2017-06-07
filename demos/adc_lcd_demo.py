52369ca6a820fdb95f81489cff2fbac8829a980c
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2017-06-07 11:38:46 +0200

    fix and improve code on ADC LCD demo
    - fixed: left sensor value is shown twice on the output
    - added module docstring
    - sorted imports
    - cleanup GPIO ports

--- demos/adc_lcd_demo.py
+++ demos/adc_lcd_demo.py
@@ -1,7 +1,15 @@
+"""
+Show sensor readings on the LCD.
+
+Repeatedly read left and right Sharp sensor values from the ADC module and
+display them on the LCD.
+"""
 from smbus import SMBus
-from LCD import Display
-from adc import ADC
 from time import sleep
+import RPi.GPIO as GPIO
+
+from adc import ADC
+from LCD import Display
 
 d = Display()
 d.init()
@@ -13,16 +21,18 @@ bus = SMBus(bus_number)
 a = ADC(bus, bus_address)
 
 left_channel = 0
-right_channel = 3
+right_channel = 1
 
 refresh_interval = 0.1
 
 try:
     while True:
-        left, right = a.read_channel(left_channel), a.read_channel(right_channel)
+        left = a.read_channel(left_channel)
+        right = a.read_channel(right_channel)
+
         d.cursor_goto_line(0)
-        d.print_string('L {0:>4d}    R {0:>4d}'.format(left, right))
+        d.print_string('L {0:>4d}    R {1:>4d}'.format(left, right))
+
         sleep(refresh_interval)
-        
 except KeyboardInterrupt:
-    pass
+    GPIO.cleanup()
