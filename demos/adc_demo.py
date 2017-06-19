17c53300251084e09ab75134f433a4fe3ebe185e
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2017-06-19 08:53:02 +0200

    fix indentation on ADC demos

--- demos/adc_demo.py
+++ demos/adc_demo.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from adc import ADC
 from smbus import SMBus
 from time import sleep
@@ -11,9 +8,9 @@ adc = ADC(bus)
 # Kontinuierliche Ausgabe der Sensorwerte auf der Standardausgabe, bis das
 # Programm mit Strg+C beendet wird
 try:
-	while True:
-		left, right = adc.read_channel(0), adc.read_channel(1)
-		print("{0:>5d} {1:>5d}".format(left, right))
-		sleep(0.2)
+    while True:
+        left, right = adc.read_channel(0), adc.read_channel(1)
+        print("{0:>5d} {1:>5d}".format(left, right))
+        sleep(0.2)
 except KeyboardInterrupt:
-	print('Bye')
+    print('Bye')
